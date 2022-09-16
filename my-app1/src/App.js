451f83c4de2989db54da9ca1391db847d91235d7
soichichi <souichi.t425@gmail.com>
2022-09-16 12:14:51 +0900

    最初のreact編集

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -7,11 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/*Edit <code>src/App.js</code> and save to reload.*/}
+          Hello 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjsorg"
           target="_blank"
           rel="noopener noreferrer"
         >
